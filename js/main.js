e4b2d6ae0959797da80966c67e04bd161f049f49
ksugawara61 <katsuya@kokohadoko.net>
2018-08-26 00:20:30 +0900

    change the results String to Array.

--- js/main.js
+++ js/main.js
@@ -4,25 +4,9 @@
     let btn = document.getElementById('btn');
 
     btn.addEventListener('click', function() {
-        let n = Math.floor(Math.random() * 3);
-        switch (n) {
-            case 0:
-                this.textContent = '大吉';
-                break;
-            case 1:
-                this.textContent = '中吉';
-                break;
-            case 2:
-                this.textContent = '凶';
-                break;
-        }
-/*        if (n === 0) {
-            this.textContent = '大吉';
-        } else if (n === 1) {
-            this.textContent = '中吉';
-        } else {
-            this.textContent = '凶';
-        }*/
+        let results = ['大吉', '中吉', '凶', '末吉'];
+        let n = Math.floor(Math.random() * results.length);
+        this.textContent = results[n];
     });
     btn.addEventListener('mousedown', function() {
         this.className = 'pushed';
