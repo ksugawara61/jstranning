9676a40e65fa09a26a7a3ecd98df7f7ca0d3b1dd
ksugawara61 <katsuya@kokohadoko.net>
2018-08-26 00:26:59 +0900

    change the probability of Omikuji

--- js/main.js
+++ js/main.js
@@ -4,9 +4,18 @@
     let btn = document.getElementById('btn');
 
     btn.addEventListener('click', function() {
-        let results = ['大吉', '中吉', '凶', '末吉'];
+        let n = Math.random();
+        if (n < 0.05) {
+            this.textContent = '大吉';
+        } else if (n < 0.2) {
+            this.textContent = '中吉';
+        } else {
+            this.textContent = '凶';
+        }
+
+        /*let results = ['大吉', '中吉', '凶', '末吉'];
         let n = Math.floor(Math.random() * results.length);
-        this.textContent = results[n];
+        this.textContent = results[n];*/
     });
     btn.addEventListener('mousedown', function() {
         this.className = 'pushed';
